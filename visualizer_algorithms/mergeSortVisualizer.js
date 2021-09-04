84a0efcfe11319dd71401fbf59e2eb7a8dcf2596
exismys <riteshzeronity@gmail.com>
2021-09-04 09:15:27 +0530

    Updated mergeSortVisualizer so that it can be controled by a speed parameter

--- visualizer_algorithms/mergeSortVisualizer.js
+++ visualizer_algorithms/mergeSortVisualizer.js
@@ -1,15 +1,15 @@
 import {greenish, radish, greyish, lightGreyish, timer} from "./globalConstants.js"
 
-let merge =  async function(array, elements, start, mid, end) {
+let merge =  async function(array, elements, speed, start, mid, end) {
     let leftArray = [], rightArray = [];
     for (let i = start, j = 0; i <= mid; i++) leftArray[j++] = array[i];
     for (let i = mid + 1, j = 0; i <= end; i++) rightArray[j++] = array[i];
     let i = 0, j = 0, current = start;
     while (i < leftArray.length && j < rightArray.length) {
-        await timer(2);
+        await timer(speed / 5);
         elements[start + i].style.backgroundColor = greyish;
         elements[mid + 1 + j].style.backgroundColor = greyish;
-        await timer(2);
+        await timer(speed / 5);
         elements[start + i].style.backgroundColor = lightGreyish;
         elements[mid + 1 + j].style.backgroundColor = lightGreyish;
         if (leftArray[i] < rightArray[j]) {
@@ -22,29 +22,29 @@ let merge =  async function(array, elements, start, mid, end) {
     }
     while (i < leftArray.length) {
         elements[current].style.backgroundColor = greyish;
-        await timer(2);
+        await timer(speed / 5);
         elements[current].style.backgroundColor = lightGreyish;
         elements[current].style.height = `${leftArray[i]}%`;
         array[current++] = leftArray[i++];
     }
     while (j < rightArray.length) {
         elements[current].style.backgroundColor = greyish;
-        await timer(2);
+        await timer(speed / 5);
         elements[current].style.backgroundColor = lightGreyish;
         elements[current].style.height = `${rightArray[j]}%`;
         array[current++] = rightArray[j++];
     }
 }
 
-let mergeSort = async function(array, elements, start, end) {
+let mergeSort = async function(array, elements, speed, start, end) {
     if (start < end) {
         let mid = Math.floor((start + end) / 2);
         elements[mid].style.backgroundColor = radish;
-        await timer(2);
+        await timer(speed);
         elements[mid].style.backgroundColor = lightGreyish;
-        await mergeSort(array, elements, start, mid);
-        await mergeSort(array, elements, mid + 1, end);
-        await merge(array, elements, start, mid, end);
+        await mergeSort(array, elements, speed, start, mid);
+        await mergeSort(array, elements, speed, mid + 1, end);
+        await merge(array, elements, speed, start, mid, end);
     }
 }
 
